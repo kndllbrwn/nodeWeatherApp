07b11036ee2cfe67ea47f4a203a26b59a6ea614f
kndllbrwn <kndllbrwn1914@gmail.com>
2017-01-18 21:52:40 -0500

    Set up function to take address from terminal and encode in order to make dynamic calls to weather API

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+.options({
+    a: {
+        demand:true,
+        alias: 'address',
+        description: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+//1301 lombard street
+
+fetch = encodeURIComponent(argv.a);
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${fetch}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
